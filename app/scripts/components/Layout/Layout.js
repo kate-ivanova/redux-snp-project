46284debf4758206b9672b0e8384a05c0ac625cb
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2016-06-07 11:55:07 +0300

    Add styles, change postcss-cssnext version

--- app/scripts/components/Layout/Layout.js
+++ app/scripts/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderLayout from 'components/HeaderLayout';
 import ContentLayout from 'components/ContentLayout';
 import FooterLayout from 'components/FooterLayout';
+import './Layout.css';
 
 const Layout = ({children}) => (
   <div className="layout">
